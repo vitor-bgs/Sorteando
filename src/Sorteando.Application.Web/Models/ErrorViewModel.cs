26dd399afc2434fcdb7b571359ff6ee8257692bc
Vitor Breno Gomes da Silva <vitorgomes@procobservicos.com>
2021-03-25 20:23:25 -0300

    Estrutura inicial de projeto

---
+++ src/Sorteando.Application.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sorteando.Application.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
